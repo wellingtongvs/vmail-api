4a0f601a4ac99872b32c1068c65266a2fee9f394
Wellington Gabriel Vicente de Souza <wellingtongabrielvs@gmail.com>
2023-04-17 21:43:58 -0300

    [feature/emails] add basic crud resolver methods

---
+++ src/emails/emails.resolver.ts
@@ -0,0 +1,45 @@
+import { Resolver, Query, Mutation, Args, ID } from '@nestjs/graphql';
+import { EmailsService } from './emails.service';
+import { Email } from './entities/email.entity';
+import { CreateEmailInput } from './dto/create-email.input';
+import { UpdateEmailInput } from './dto/update-email.input';
+import { PubSub } from 'graphql-subscriptions';
+import { FindAllEmailsInput } from './dto/find-all-emails.input';
+import { FindAllEmailsOutput } from './dto/find-all-emails.output';
+
+const pubSub = new PubSub();
+
+@Resolver(() => Email)
+export class EmailsResolver {
+  constructor(private readonly emailsService: EmailsService) {}
+
+  @Mutation(() => Email)
+  async createEmail(@Args('input') createEmailInput: CreateEmailInput) {
+    const draft = this.emailsService.create(createEmailInput);
+    pubSub.publish('emailDraftAdded', draft);
+    return draft;
+  }
+
+  @Mutation(() => Email)
+  async updateEmail(
+    @Args('updateEmailInput') updateEmailInput: UpdateEmailInput,
+  ) {
+    return this.emailsService.update(`updateEmailInput.id`, updateEmailInput);
+  }
+
+  @Query(() => FindAllEmailsOutput, { name: 'emails' })
+  async findAll(@Args('filters') filters: FindAllEmailsInput) {
+    const response = await this.emailsService.findAll(filters);
+    return response;
+  }
+
+  @Query(() => Email, { name: 'email' })
+  async findOne(@Args('id', { type: () => ID }) id: string) {
+    return this.emailsService.findOne(id);
+  }
+
+  @Mutation(() => Email)
+  async deleteEmail(@Args('id', { type: () => ID }) id: string) {
+    return this.emailsService.deleteEmail(id);
+  }
+}
